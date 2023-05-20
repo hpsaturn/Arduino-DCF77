fbe6bbf4b76202fed60651790c14d6cc27243ad9
Hpsaturn <hpsaturn@gmail.com>
2023-05-20 23:24:40 +0200

    disable LED builtin for compatibility issues with other boards

--- utility/Utils.cpp
+++ utility/Utils.cpp
@@ -1,55 +1,49 @@
 #include "Utils.h"
 
 namespace Utils {
-	
-#define DEBUG_BLINK_PIN 13	     // Connected to debug led
+
+//#define DEBUG_BLINK_PIN 13   // Connected to debug led
 //#define VERBOSE_DEBUG 1	     // Verbose
 
-	void LogLn(const char*s)
-	{
-	#ifdef VERBOSE_DEBUG
-		Serial.println(s);
-	#endif
-	}
-
-	void Log(const char*s)
-	{
-	#ifdef VERBOSE_DEBUG
-	  Serial.print(s);
-	#endif
-	}
-	void Log(int i,char format)
-	{
-	#ifdef VERBOSE_DEBUG
-	  Serial.print(i, format);
-	#endif
-	}
-
-	void LogLn(int i,char format)
-	{
-	#ifdef VERBOSE_DEBUG
-	  Serial.println(i, format);
-	#endif
-	}
-
-	void Log(int i)
-	{
-	#ifdef VERBOSE_DEBUG
-	  Serial.print(i);
-	#endif
-	}
-
-	void LogLn(int i)
-	{
-	#ifdef VERBOSE_DEBUG
-	  Serial.println(i);
-	#endif
-	}
-       
-    void BlinkDebug(uint8_t state) {
-    #ifdef DEBUG_BLINK_PIN
-        digitalWrite(DEBUG_BLINK_PIN, state);
-    #endif
-	}
-	
+void LogLn(const char* s) {
+#ifdef VERBOSE_DEBUG
+  Serial.println(s);
+#endif
 }
+
+void Log(const char* s) {
+#ifdef VERBOSE_DEBUG
+  Serial.print(s);
+#endif
+}
+void Log(int i, char format) {
+#ifdef VERBOSE_DEBUG
+  Serial.print(i, format);
+#endif
+}
+
+void LogLn(int i, char format) {
+#ifdef VERBOSE_DEBUG
+  Serial.println(i, format);
+#endif
+}
+
+void Log(int i) {
+#ifdef VERBOSE_DEBUG
+  Serial.print(i);
+#endif
+}
+
+void LogLn(int i) {
+#ifdef VERBOSE_DEBUG
+  Serial.println(i);
+#endif
+}
+
+void BlinkDebug(uint8_t state) {
+#ifdef DEBUG_BLINK_PIN
+  digitalWrite(DEBUG_BLINK_PIN, state);
+#endif
+}
+
+}  // namespace Utils
